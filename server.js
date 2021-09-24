a4bd57c78360e0df42da2789cda3cdaa611fe09c
DESKTOP-50417C0\chengchen shen <shenchen@deakin.edu.au>
2021-09-24 16:42:46 +1000

    1

--- server.js
+++ server.js
@@ -261,7 +261,7 @@ const GOOGLE_CLIENT_SECRET = '1VcNM0DFTnqSd0zF3077G81q';
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:8000/auth/google/callback"
+    callbackURL:"https://evening-retreat-13853.herokuapp.com/auth/google/callback"
     },
     function(accessToken,refreshToken,profile,done){
         userProfile = profile;
